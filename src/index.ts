43bda20391143173cb6d6fde3390d143176e5522
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-08 17:27:30 +0300

    simple server created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello');
+import express, { Express } from "express";
+
+class Main {
+
+    static #instance: Main;
+
+    static get(): Main {
+        if (!this.#instance) {
+            this.#instance = new Main();
+        }
+        return this.#instance;
+    }
+
+    #server: Express = express();
+    port: number = 9090;
+
+    constructor() {
+        
+        this.#server.listen(this.port, () => {
+            console.log(`The server is running on: ${this.port}`);
+        })
+    }
+}
+
+Main.get();
